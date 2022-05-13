5b28515f98828e3ccb7395b5f21316bc36f0e307
Husow070 <71512853+Husow070@users.noreply.github.com>
2022-05-13 14:48:01 +0200

    Add files via upload

---
+++ Healthone/Main.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<head>
+    <title>Healthone</title>
+</head>
+<body>
+<img src="./Foto/healthonelogo.png" alt="logo" style="width:355px;">
+<div class="healthOnebg">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Sportcenter</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sportcenter</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">sportapparaat</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">registreren</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+
+</body>
+</html>
